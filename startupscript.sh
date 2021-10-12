d8467cf566063fde273ab9980b069b85d8d1a307
SCV2PL <92363849+SCV2PL@users.noreply.github.com>
2021-10-12 12:21:33 +0200

    Delete startupscript.sh

--- startupscript.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-python3 /home/luke_blue_lox/clear_todaysum.py
-
-python3 /home/luke_blue_lox/update_todaysum.py
-
-python3 /home/luke_blue_lox/clear.py
-
-python3 /home/luke_blue_lox/update.py
